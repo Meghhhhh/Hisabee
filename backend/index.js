c2c31a2e6d3ab66bbc95f65b3891c47811ec961d
Megh Prajapati <meghprajapati009@gmail.com>
2025-04-24 00:35:02 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}`));
