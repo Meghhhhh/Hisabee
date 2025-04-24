6d29a373e7c178e8fac3f1ea8a19fdb766089615
Megh Prajapati <meghprajapati009@gmail.com>
2025-04-24 17:53:05 +0530

    Postgres setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import 'dotenv/config'
 import express from 'express';
+import pool from './config/postgres-db.js';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', async (req, res) => {
+    const query = "SELECT NOW()"
+    const result = await pool.query(query);
+    console.log(result.rows[0].now);
+    res.json("Hello worls")
+});
 
 app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}`));
