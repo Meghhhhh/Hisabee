4187c5223a4d3bf5ccbd0e758997a08211163ab2
Megh Prajapati <meghprajapati009@gmail.com>
2025-05-12 16:57:29 +0530

    Notification table added

--- backend/database/data.js
+++ backend/database/data.js
@@ -2,9 +2,10 @@ const createTableQueries = {
   users: `
       CREATE TABLE IF NOT EXISTS users (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        email VARCHAR(60) NOT NULL,
+        email VARCHAR(60) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
-        phone_number VARCHAR(10),
+        name VARCHAR(255),
+        phone_number VARCHAR(10) UNIQUE,
         payment_reference VARCHAR(255),
         created_at TIMESTAMP DEFAULT NOW()
       )
@@ -13,6 +14,7 @@ const createTableQueries = {
       CREATE TABLE IF NOT EXISTS friends (
         user_id UUID REFERENCES users(id) ON DELETE CASCADE,
         friend_id UUID REFERENCES users(id) ON DELETE CASCADE,
+        created_at TIMESTAMP DEFAULT NOW()
         status VARCHAR(20) CHECK (status IN ('pending', 'accepted', 'blocked')),
         PRIMARY KEY (user_id, friend_id)
       );
@@ -20,7 +22,7 @@ const createTableQueries = {
   hisabs: `
       CREATE TABLE IF NOT EXISTS hisabs (
         hisab_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        created_by UUID REFERENCES users(id) ON DELETE CASCADE,
+        created_by UUID REFERENCES users(id) ON DELETE CASCADE NOT NULL,
         title VARCHAR(255) NOT NULL,
         total_budget DECIMAL(10, 2),
         created_at TIMESTAMP DEFAULT NOW()
@@ -61,6 +63,17 @@ const createTableQueries = {
         pdf_url VARCHAR(255),
         qr_code_link VARCHAR(255)
       );
+    `,
+  notifications: `
+      CREATE TABLE IF NOT EXISTS notifications(
+        notification_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        is_read BOOLEAN DEFAULT FALSE,
+        user_id UUID REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
+        message TEXT NOT NULL,
+        type VARCHAR(50) CHECK(type IN ('FRIEND','HISAB_REQUEST', 'OTHER')),
+        title TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+      )
     `,
 };
 
