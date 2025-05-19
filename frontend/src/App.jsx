308d18948e9de9e9cd4a130bfc9390b954f32d86
Megh Prajapati <meghprajapati009@gmail.com>
2025-05-19 17:12:53 +0530

    Connected signup remaining is seting up of cookies 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,33 @@
 import React from 'react';
-
 import Home from './pages/Home';
-import { RouterProvider, createBrowserRouter } from 'react-router';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import Register from './pages/Register';
 import Notifications from './pages/Notifications';
 import OTP from './pages/OTP';
 
+import { RouterProvider, createBrowserRouter } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/home",
-      element: <Home />
-    },
-    {
-      path: "/profile",
-      element: <Profile />
-    },
-    {
-      path: "/signup",
-      element: <SignUp />
-    },
-    {
-      path: "/register",
-      element: <Register />
-    },
-    {
-      path: "/notifications",
-      element: <Notifications />
-    },
-    {
-      path: "/otp",
-      element: <OTP />
-    }
-    
+    { path: '/', element: <Home /> },
+    { path: '/home', element: <Home /> },
+    { path: '/profile', element: <Profile /> },
+    { path: '/signup', element: <SignUp /> },
+    { path: '/register', element: <Register /> },
+    { path: '/notifications', element: <Notifications /> },
+    { path: '/otp', element: <OTP /> },
   ]);
   
 
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer theme="dark" />
+    </>
+  );
 };
 
 export default App;
