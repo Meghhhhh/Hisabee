3181ad80abcd550371f36ce03df9b17c4d3911c6
Megh Prajapati <meghprajapati009@gmail.com>
2025-07-20 22:13:41 +0530

    Bugs

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,9 +3,15 @@ import { useSelector } from 'react-redux';
 
 const ProtectedRoute = () => {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const user = useSelector(state => state.user);
+  console.log(user);
 
   if (isLoggedIn === null) {
     return `Loading...`;
+  }
+
+  if (!user.user_id) {
+    return <Navigate to="/home" replace />;
   }
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/signup" replace />;
--- frontend/store/slice/userSlice.js
+++ frontend/store/slice/userSlice.js
@@ -3,32 +3,33 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
   name: 'user',
   initialState: {
+    user_id: null,
     name: null,
     email: null,
     payment_refrence: null,
-    phone: null,
+    phone_number: null,
     avatar: './avatar.png',
-    history: [],
     friends: [],
-    notifications: [],
-    friendRequests: [],
+    hisabs: [],
   },
   reducers: {
     setUserData: (state, action) => {
       const {
+        user_id,
         name,
         email,
         payment_refrence,
-        phone,
-        history = [],
+        phone_number,
         friends = [],
+        hisabs = [],
       } = action.payload;
+      state.user_id = user_id;
       state.name = name;
       state.email = email;
       state.payment_refrence = payment_refrence;
-      state.phone = phone;
-      state.history = history;
+      state.phone_number = phone_number;
       state.friends = friends;
+      state.hisabs = hisabs;
     },
     setNotifications: (state, action) => {
       state.notifications = action.payload;
