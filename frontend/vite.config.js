3bdd11b35c0b096b15a048d5c07f57438b9a9cb9
HARSHIT5114 <rajputharshit5114@gmail.com>
2025-07-22 20:36:09 +0530

    Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0' // important for Render
+  }
 });
