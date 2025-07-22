91208de0b3658ea406bd5f6f7e3c3c153a20c581
Megh Prajapati <meghprajapati009@gmail.com>
2025-07-23 00:05:41 +0530

    Ui

--- frontend/src/components/Profile/ProfileOverview.jsx
+++ frontend/src/components/Profile/ProfileOverview.jsx
@@ -6,11 +6,11 @@ const ProfileOverview = () => {
   return (
     <div className="font-[Montserrat] relative">
       {/* Avatar and Name */}
-      <div className="flex items-center">
+      <div className="flex flex-col md:flex-row items-center md:items-start">
         <img
           src={user.avatar}
           alt="User avatar"
-          className="w-52 h-52 rounded-full shadow-2xl border-4 border-black"
+          className="w-32 h-32 md:w-52 md:h-52 rounded-full shadow-2xl border-4 border-black"
         />
         <h2 className="text-3xl md:text-5xl font-bold mt-4 md:mt-15 md:ml-5 ">
           {user.name}
@@ -18,9 +18,9 @@ const ProfileOverview = () => {
       </div>
 
       {/* Contact Section */}
-      <div className="flex my-6 space-x-4">
+      <div className="flex flex-col md:flex-row my-6 md:space-x-4">
         {/* Contact Info */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-gray-700 w-[50%] ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-gray-700 w-full md:w-[50%] ">
           <div className="">
             <h4 className="text-sm text-gray-500 ">Email</h4>
             <p className="font-medium">{user.email}</p>
@@ -43,16 +43,17 @@ const ProfileOverview = () => {
             Edit Info
           </button>
         </Link>
-        <div className="flex flex-col items-center">
-          <h4 className="text-base md:text-lg text-gray-500 text-center">
-            QR code
-          </h4>
-          <img
-            src="/Subject2.png"
-            alt=""
-            className="border-4 border-black h-[250px] mx-auto"
-          />
-        </div>
+      </div>
+
+      <div className="mt-8 md:absolute md:top-18 md:right-1.5 w-full md:w-auto flex flex-col items-center">
+        <h4 className="text-base md:text-lg text-gray-500 text-center">
+          QR code
+        </h4>
+        <img
+          src="/Subject2.png"
+          alt=""
+          className="border-4 border-black h-40 md:h-[250px] mx-auto"
+        />
       </div>
     </div>
   );
